82f624ff8940d455d46bfb8f8729d914879851d6
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-01-13 22:19:26 +0900

    完成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import React from 'react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>All Services</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,41 @@
 import React from 'react';
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import services from '../services.json';
 
 export default function Home(): React.ReactElement {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+    <>
+      <main className={styles.container}>
+        <h1 className={styles.title}>All Services</h1>
+        <ul className={styles.cards}>
+          {services.map((s) => (
+            <li key={s.name} className={styles.cardContainer}>
+              <div className={styles.card}>
+                <a
+                  className={styles.link}
+                  href={s.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {s.name}
+                  <br />
+                  <small className={styles.url}>{s.url}</small>
+                </a>
+                <p className={styles.description}>{s.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </main>
-
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://tinykitten.me"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Copyright &copy; 2021 TinyKitten
         </a>
       </footer>
-    </div>
+    </>
   );
 }
